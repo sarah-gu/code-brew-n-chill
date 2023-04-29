30066f38a36e42a0fd58beff9091191ce8efb9d9
joyhe208 <joyhe208@gmail.com>
2023-04-29 18:09:32 -0400

    took src out of pages directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Landing } from "./src/Landing";
+import { Landing } from "../src/Landing";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
