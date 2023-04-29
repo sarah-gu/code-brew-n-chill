ea33d4be11d2351e904024785cd7d80a987712ed
joyhe208 <joyhe208@gmail.com>
2023-04-29 18:49:29 -0400

    added convex provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/global.css'
 
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ConvexProvider client={convex}>
+          <Component {...pageProps} />;
+        </ConvexProvider>
 }
